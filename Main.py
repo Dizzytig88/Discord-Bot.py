3389f0ae30e2d7420a69f7d3513c693baea2fdc1
Dizzytig88 <83091189+Dizzytig88@users.noreply.github.com>
2021-06-16 14:07:25 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-	await client.change_presence(activity=discord.Game('#this will make it so your bots status will be playing ____'))
+	await client.change_presence(activity=discord.Game('#this will make it so your bots status will be playing ____(optional)'))
 
 	print ('We have logged in as {0.user}'.format(client))
   
